7f765a7f2ba565cdca5c559cbea63bc49aa4ba3d
piotrzalecki <piotrzalecki@gmail.com>
2019-10-19 20:18:20 +0100

    initial commit

---
+++ src/main/java/com/piotrzalecki/homeserver/HomeserverApplication.java
@@ -0,0 +1,13 @@
+package com.piotrzalecki.homeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeserverApplication.class, args);
+	}
+
+}
